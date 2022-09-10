3f9226e1b4fe4c1a95f59cfbaa574431ca878283
Shady Abdelhamid <shady.abdelhamid@capiter.net>
2022-09-10 03:33:02 +0200

    feat: add form control implementation

--- src/app/chip-input/chip-input.component.ts
+++ src/app/chip-input/chip-input.component.ts
@@ -1,30 +1,53 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { __values } from 'tslib';
 
 @Component({
   standalone: true,
   selector: 'chip-input',
   templateUrl: './chip-input.component.html',
   styleUrls: ['./chip-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ChipInputComponent,
+      multi: true
+    }],
   imports: [BrowserModule]
 })
-export class ChipInputComponent implements OnInit {
+export class ChipInputComponent implements OnInit, ControlValueAccessor {
   values: string[] = [];
-  suggestions: string[] = [
-    'Typescript',
-    'Javascript',
-    'C#',
-    'Objective C',
-    'C++',
-    'Python',
-    'Java',
-    'Perl'
-  ]
+  @Input() suggestions!: string[];
+
+  @Output() selection = new EventEmitter<Array<string>>();
 
   @ViewChild('textbox', { static: true }) textbox!: ElementRef;
+  field = '';
+  isDisabled = true;
+
+  set value(val: string) {
+    this.field = val
+    this.onChange(val)
+    this.onTouch(val)
+  }
 
   constructor() { }
+
+  onChange: any = () => { }
+  onTouch: any = () => { }
+
+  writeValue(value: any) {
+    this.value = value
+  }
+  registerOnChange(fn: any) {
+    this.onChange = fn
+  }
+  registerOnTouched(onTouched: Function) {
+    this.onTouch = onTouched;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
 
   ngOnInit(): void {
   }
@@ -32,6 +55,7 @@ export class ChipInputComponent implements OnInit {
   onEnterKeydown() {
     const { value } = this.textbox.nativeElement;
     this.values.push(value);
+    this.selection.emit(this.values);
     this.textbox.nativeElement.value = '';
   }
 
